05e6adcd97843e1860d666defc8442f58c9f5815
Tarcisio <tarcisioandrade016@gmail.com>
2024-07-02 11:33:23 -0300

    feat: jwt encrypt

---
+++ src/lib/jwt.ts
@@ -0,0 +1,19 @@
+import env from "@/env";
+import { SignJWT, jwtVerify } from "jose";
+
+const secretKey = env.JWT_SECRET;
+const key = new TextEncoder().encode(secretKey);
+
+export async function encrypt(payload: any) {
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .sign(key);
+}
+
+export async function decrypt(input: string): Promise<any> {
+  const { payload } = await jwtVerify(input, key, {
+    algorithms: ["HS256"],
+  });
+  return payload;
+}
