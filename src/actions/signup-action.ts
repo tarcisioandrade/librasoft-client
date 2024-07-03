606db2310dea29c74752e389e1e0c3b069363bd4
Tarcisio <tarcisioandrade016@gmail.com>
2024-07-03 15:59:11 -0300

    feat: signup screen

---
+++ src/actions/signup-action.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { signup } from "@/services/session";
+import { redirect } from "next/navigation";
+
+export async function signupAction(_prevState: any, formData: FormData) {
+  const result = await signup(formData);
+
+  if (result.success === false) {
+    return result;
+  }
+
+  redirect("/signin");
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,23 @@
+import FormSignup from "@/components/form-signup";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="mx-auto mt-12 w-[500px]">
+      <Link href="/" className="block text-center text-2xl font-bold">
+        LibraSoft
+      </Link>
+      <FormSignup />
+      <div className="mt-6 flex flex-col items-center gap-4">
+        <p className="text-sm text-muted-foreground">Já tem uma conta?</p>
+        <Button variant="secondary" className="w-full" asChild>
+          <Link href="/signin">Faça Login</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
