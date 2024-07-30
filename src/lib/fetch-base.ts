29bf8be74ba45ac212915d97cd57b78554891b7a
Tarcisio <tarcisioandrade016@gmail.com>
2024-07-29 22:11:58 -0300

    refactor: fetch base now use and export FetchErrorResponse type

--- src/lib/fetch-base.ts
+++ src/lib/fetch-base.ts
@@ -1,8 +1,15 @@
-import env from "@/env";
 import { createFetch } from "@better-fetch/fetch";
 import { z } from "zod";
 
-export const $fetch = createFetch({
-  baseURL: env.BACKEND_URL,
-  defaultError: z.object({ errors: z.array(z.string()) }),
+const fetchErrorSchema = z.object({
+  errors: z.array(z.string()),
+  message: z.string(),
+  status: z.number(),
+  statusText: z.string(),
 });
+
+export const $fetch = createFetch({
+  defaultError: fetchErrorSchema,
+});
+
+export type FetchErrorResponse = z.infer<typeof fetchErrorSchema>;
