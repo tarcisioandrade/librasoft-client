9a3f9d4d417947ed4f2d06b40e1d5410532dea75
Tarcisio <tarcisioandrade016@gmail.com>
2024-07-29 22:10:15 -0300

    feat: logging fetch url in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { hostname: "github.com" },
-      { hostname: "m.media-amazon.com" },
-    ],
+    remotePatterns: [{ hostname: "github.com" }, { hostname: "m.media-amazon.com" }],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
   },
 };
 
