56f66388b0b1ff3c3a08c90b9b8adf7ce6043c11
Tarcisio <tarcisioandrade016@gmail.com>
2024-09-13 10:20:41 -0300

    fix: logout in rent details page pushing user to not found page

---
+++ src/actions/auth/logout.action.ts
@@ -0,0 +1,9 @@
+"use server";
+
+import { logout } from "@/services/session.service";
+import { redirect } from "next/navigation";
+
+export async function logouAction() {
+  logout();
+  redirect("/");
+}
