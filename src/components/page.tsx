fce4aa3b4fb27a660ba3cd327a33598695436209
Tarcisio <tarcisioandrade016@gmail.com>
2024-09-23 11:08:47 -0300

    feat: page container component

---
+++ src/components/page.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import React, { ComponentPropsWithRef, forwardRef } from "react";
+import { Slot } from "@radix-ui/react-slot";
+
+interface Props extends ComponentPropsWithRef<"div"> {
+  withHeader?: boolean;
+  asChild?: boolean;
+  container: "container" | "container-secondary" | "container-third";
+}
+
+const Page = forwardRef<HTMLDivElement, Props>(
+  ({ className, children, asChild, container, withHeader = true, ...props }, ref) => {
+    const Comp = asChild ? Slot : "div";
+
+    return (
+      <Comp
+        ref={ref}
+        className={cn(container, { "mt-[105px] lg:mt-0": withHeader }, className)}
+        {...props}
+      >
+        {children}
+      </Comp>
+    );
+  },
+);
+
+export default Page;
