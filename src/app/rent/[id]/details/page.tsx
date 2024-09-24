d924790190d07086325fec528b8af9bf5f7e8ebc
Tarcisio <tarcisioandrade016@gmail.com>
2024-09-24 11:43:53 -0300

    styles: responsivity implementation in rent page

--- src/app/rent/[id]/details/page.tsx
+++ src/app/rent/[id]/details/page.tsx
@@ -14,6 +14,7 @@ import React from "react";
 import AlertCancelRent from "../../components/alert-cancel-rent";
 import { Metadata } from "next";
 import { generateCustomMetadata } from "@/utils/generate-custom-metadata";
+import Page from "@/components/page";
 
 export const metadata: Metadata = generateCustomMetadata("Detalhes");
 
@@ -30,147 +31,165 @@ const RentDetailsPage = async ({ params }: { params: { id: string } }) => {
   return (
     <>
       <Header />
-      <section className="container-third my-6">
-        <div className="border">
-          <header className="w-full bg-secondary px-4 py-2">
-            <p className="text-sm text-muted-foreground">Status da Solicitação</p>
-          </header>
-          <div className="flex items-start justify-around gap-12 p-4">
-            {rent.data.status === "Rent_Canceled" ? (
-              <div className={cn("flex flex-col items-center justify-center gap-2 text-red-700")}>
-                <X size={36} />
-                <div className="text-center">
-                  <p className="text-xs font-semibold">Cancelado</p>
+      <Page container="container-third" asChild>
+        <section>
+          <div className="mt-6 border">
+            <header className="w-full bg-secondary px-4 py-2">
+              <p className="text-sm text-muted-foreground">Status da Solicitação</p>
+            </header>
+            <div className="overflow-x-scroll sm:overflow-auto">
+              <div className="flex w-[550px] items-start justify-around gap-8 p-4 sm:w-full sm:gap-12">
+                {rent.data.status === "Rent_Canceled" ? (
+                  <div
+                    className={cn("flex flex-col items-center justify-center gap-2 text-red-700")}
+                  >
+                    <X size={36} />
+                    <div className="text-center">
+                      <p className="text-xs font-semibold">Cancelado</p>
+                    </div>
+                  </div>
+                ) : (
+                  <div
+                    className={cn("flex flex-col items-center justify-center gap-2 text-green-500")}
+                  >
+                    <Box size={36} />
+                    <div className="text-center">
+                      <p className="text-xs font-semibold">Solicitado</p>
+                      <p className="text-xs">
+                        {formatDate(rent.data.rentDate, { dateStyle: "short" })} :{" "}
+                        {formatTime(rent.data.rentDate)}
+                      </p>
+                    </div>
+                  </div>
+                )}
+                <div
+                  className={cn(
+                    "flex flex-col items-center justify-center gap-2 text-muted-foreground",
+                    rent.data.status === "Requested_Awaiting_Pickup" && "text-yellow-500",
+                    rent.data.status !== "Rent_Canceled" &&
+                      rent.data.status !== "Requested_Awaiting_Pickup" &&
+                      "text-green-500",
+                  )}
+                >
+                  <Clock size={36} />
+                  <div className="text-center">
+                    <p className="text-xs font-semibold">{ERentStatus.Requested_Awaiting_Pickup}</p>
+                  </div>
                 </div>
-              </div>
-            ) : (
-              <div className={cn("flex flex-col items-center justify-center gap-2 text-green-500")}>
-                <Box size={36} />
-                <div className="text-center">
-                  <p className="text-xs font-semibold">Solicitado</p>
-                  <p className="text-xs">
-                    {formatDate(rent.data.rentDate, { dateStyle: "short" })} :{" "}
-                    {formatTime(rent.data.rentDate)}
-                  </p>
-                </div>
-              </div>
-            )}
-            <div
-              className={cn(
-                "flex flex-col items-center justify-center gap-2 text-muted-foreground",
-                rent.data.status === "Requested_Awaiting_Pickup" && "text-yellow-500",
-                rent.data.status !== "Rent_Canceled" &&
-                  rent.data.status !== "Requested_Awaiting_Pickup" &&
-                  "text-green-500",
-              )}
-            >
-              <Clock size={36} />
-              <div className="text-center">
-                <p className="text-xs font-semibold">{ERentStatus.Requested_Awaiting_Pickup}</p>
+                {rent.data.status === "Rent_Expired" ? (
+                  <div
+                    className={cn("flex flex-col items-center justify-center gap-2 text-red-500")}
+                  >
+                    <Clock size={36} />
+                    <div className="text-center">
+                      <p className="text-xs font-semibold">{ERentStatus.Rent_Expired}</p>
+                      <p className="text-xs font-semibold">Uma punição será aplicada</p>
+                    </div>
+                  </div>
+                ) : (
+                  <div
+                    className={cn(
+                      "flex flex-col items-center justify-center gap-2 text-muted-foreground",
+                      rent.data.status === "Rent_Finished" && "text-green-500",
+                      rent.data.status === "Rent_In_Progress" && "text-yellow-500",
+                    )}
+                  >
+                    <BookOpenText size={36} />
+                    <div className="text-center">
+                      <p className="text-xs font-semibold">{ERentStatus.Rent_In_Progress}</p>
+                    </div>
+                  </div>
+                )}
+                {isReturnLate ? (
+                  <div
+                    className={cn(
+                      "flex flex-col items-center justify-center gap-2 text-muted-foreground text-red-500",
+                    )}
+                  >
+                    <Check size={36} />
+                    <div className="text-center">
+                      <p className="text-xs font-semibold">
+                        {ERentStatus.Rent_Finished} com Atraso
+                      </p>
+                      <p className="text-xs">
+                        {formatDate(rent.data.returnedDate!, { dateStyle: "short" })}
+                      </p>
+                      <p className="text-xs">Uma punição foi aplicada!</p>
+                    </div>
+                  </div>
+                ) : (
+                  <div
+                    className={cn(
+                      "flex flex-col items-center justify-center gap-2 text-muted-foreground",
+                      rent.data.status === "Rent_Finished" && "text-green-500",
+                    )}
+                  >
+                    <Check size={36} />
+                    <div className="text-center">
+                      <p className="text-xs font-semibold">{ERentStatus.Rent_Finished}</p>
+                      <p className="text-xs">
+                        {formatDate(rent.data.returnedDate ?? rent.data.expectedReturnDate, {
+                          dateStyle: "short",
+                        })}
+                      </p>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
-            {rent.data.status === "Rent_Expired" ? (
-              <div className={cn("flex flex-col items-center justify-center gap-2 text-red-500")}>
-                <Clock size={36} />
-                <div className="text-center">
-                  <p className="text-xs font-semibold">{ERentStatus.Rent_Expired}</p>
-                  <p className="text-xs font-semibold">Uma punição será aplicada</p>
-                </div>
-              </div>
-            ) : (
-              <div
-                className={cn(
-                  "flex flex-col items-center justify-center gap-2 text-muted-foreground",
-                  rent.data.status === "Rent_Finished" && "text-green-500",
-                  rent.data.status === "Rent_In_Progress" && "text-yellow-500",
-                )}
-              >
-                <BookOpenText size={36} />
-                <div className="text-center">
-                  <p className="text-xs font-semibold">{ERentStatus.Rent_In_Progress}</p>
-                </div>
-              </div>
-            )}
-            {isReturnLate ? (
-              <div
-                className={cn(
-                  "flex flex-col items-center justify-center gap-2 text-muted-foreground text-red-500",
-                )}
-              >
-                <Check size={36} />
-                <div className="text-center">
-                  <p className="text-xs font-semibold">{ERentStatus.Rent_Finished} com Atraso</p>
-                  <p className="text-xs">
-                    {formatDate(rent.data.returnedDate!, { dateStyle: "short" })}
-                  </p>
-                  <p className="text-xs">Uma punição foi aplicada!</p>
-                </div>
-              </div>
-            ) : (
-              <div
-                className={cn(
-                  "flex flex-col items-center justify-center gap-2 text-muted-foreground",
-                  rent.data.status === "Rent_Finished" && "text-green-500",
-                )}
-              >
-                <Check size={36} />
-                <div className="text-center">
-                  <p className="text-xs font-semibold">{ERentStatus.Rent_Finished}</p>
-                  <p className="text-xs">
-                    {formatDate(rent.data.returnedDate ?? rent.data.expectedReturnDate, {
-                      dateStyle: "short",
-                    })}
-                  </p>
-                </div>
-              </div>
-            )}
           </div>
-        </div>
 
-        <div className="mt-6 border">
-          <header className="w-full bg-secondary px-4 py-2">
-            <p className="text-sm text-muted-foreground">Resumo da Solicitação</p>
-          </header>
+          <div className="mt-6 border">
+            <header className="w-full bg-secondary px-4 py-2">
+              <p className="text-sm text-muted-foreground">Resumo da Solicitação</p>
+            </header>
 
-          <div className="flex flex-col gap-4 p-4">
-            {rent.data.books.map((book) => (
-              <div key={book.id} className="flex gap-4 border p-2">
-                <Link
-                  href={`/book/${book.id}`}
-                  target="_blank"
-                  className="h-[123px] w-24 rounded bg-muted"
-                >
-                  <Image
-                    src={book.image}
-                    width={215}
-                    height={284}
-                    alt={book.title}
-                    className="h-full"
-                  />
-                </Link>
-                <div>
-                  <ul className="space-y-1">
-                    <li className="font-semibold">
-                      <Link href={`/book/${book.id}`} target="_blank">
-                        {book.title} -{" "}
-                        <span className="text-muted-foreground">{book.publisher}</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <StarRating rating={book.averageRating} size={12} />
-                    </li>
-                    <li className="text-xs text-muted-foreground">por {book.author.name}</li>
-                    <li className="text-xs text-muted-foreground">{ECoverType[book.coverType]}</li>
-                  </ul>
+            <div className="flex flex-col gap-4 p-4">
+              {rent.data.books.map((book) => (
+                <div key={book.id} className="flex gap-4 border p-2">
+                  <Link
+                    href={`/book/${book.id}`}
+                    target="_blank"
+                    className="h-[123px] w-24 rounded bg-muted"
+                  >
+                    <Image
+                      src={book.image}
+                      width={215}
+                      height={284}
+                      alt={book.title}
+                      className="h-full"
+                    />
+                  </Link>
+                  <div className="flex-1">
+                    <ul className="space-y-1">
+                      <li className="font-semibold">
+                        <Link
+                          href={`/book/${book.id}`}
+                          target="_blank"
+                          className="line-clamp-2 text-sm sm:text-base"
+                        >
+                          {book.title} -{" "}
+                          <span className="text-muted-foreground">{book.publisher}</span>
+                        </Link>
+                      </li>
+                      <li>
+                        <StarRating rating={book.averageRating} size={12} />
+                      </li>
+                      <li className="text-xs text-muted-foreground">por {book.author.name}</li>
+                      <li className="text-xs text-muted-foreground">
+                        {ECoverType[book.coverType]}
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            {rent.data.status === "Requested_Awaiting_Pickup" ? (
+              <AlertCancelRent rentId={rent.data.id} />
+            ) : null}
           </div>
-          {rent.data.status === "Requested_Awaiting_Pickup" ? (
-            <AlertCancelRent rentId={rent.data.id} />
-          ) : null}
-        </div>
-      </section>
+        </section>
+      </Page>
     </>
   );
 };
--- src/app/rent/page.tsx
+++ src/app/rent/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/header";
+import Page from "@/components/page";
 import { Button } from "@/components/ui/button";
 import { Constants } from "@/constants";
 import { ERentStatus } from "@/enums/ERentStatus";
@@ -21,48 +22,57 @@ const RentPage = async () => {
   return (
     <>
       <Header />
-      <section className="container-third my-6">
-        <div className="mb-6">
-          <h2 className="text-3xl font-semibold leading-10">Aluguéis</h2>
-          <p className="text-sm text-muted-foreground">Acompanhe aqui seus pedidos.</p>
-        </div>
-        <div className="flex flex-col border">
-          {!rents?.data.length ? (
-            <div className="grid h-48 place-items-center border">
-              <p className="text-7xl uppercase text-muted-foreground/50">Vazio</p>
-            </div>
-          ) : null}
-          {rents?.data.map((rent, i, array) => (
-            <div key={rent.id} className={cn("p-4", array.length !== i + 1 && "border-b")}>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                  <div className="flex size-10 items-center justify-center rounded-full bg-primary">
-                    <Book size={18} color="#fff" />
-                  </div>
-                  <div className="w-[230px] space-y-1">
-                    <p className="text-sm">
-                      <strong>Pedido: </strong>
-                      {formatDate(rent.rentDate)}{" "}
-                    </p>
-                    <p className="text-sm">
-                      <strong>Retorno:</strong> {formatDate(rent.expectedReturnDate)}{" "}
-                    </p>
-                  </div>
-                </div>
-                <p
-                  className="w-[130px] justify-self-start text-xs font-semibold"
-                  style={{ color: Constants.STATUS_COLOR[rent.status] }}
-                >
-                  {ERentStatus[rent.status]}
-                </p>
-                <Button variant="secondary" className="text-xs text-muted-foreground" asChild>
-                  <Link href={`/rent/${rent.id}/details`}>Ver Detalhes</Link>
-                </Button>
+      <Page container="container-third" asChild>
+        <section>
+          <div className="mb-6">
+            <h2 className="text-3xl font-semibold leading-10">Aluguéis</h2>
+            <p className="text-sm text-muted-foreground">Acompanhe aqui seus pedidos.</p>
+          </div>
+          <div className="flex flex-col border">
+            {!rents?.data.length ? (
+              <div className="grid h-48 place-items-center border">
+                <p className="text-7xl uppercase text-muted-foreground/50">Vazio</p>
               </div>
-            </div>
-          ))}
-        </div>
-      </section>
+            ) : null}
+            {rents?.data.map((rent, i, array) => (
+              <div
+                key={rent.id}
+                className={cn("pb-4 sm:p-4", array.length !== i + 1 && "border-b")}
+              >
+                <div className="flex flex-col justify-between gap-4 sm:flex-row sm:items-center">
+                  <div className="flex items-center gap-4 bg-secondary px-4 py-2 sm:bg-transparent sm:px-0 sm:py-0">
+                    <div className="flex size-10 items-center justify-center rounded-full bg-primary">
+                      <Book size={18} color="#fff" />
+                    </div>
+                    <div className="w-[230px] space-y-1">
+                      <p className="text-sm">
+                        <strong>Pedido: </strong>
+                        {formatDate(rent.rentDate)}{" "}
+                      </p>
+                      <p className="text-sm">
+                        <strong>Retorno:</strong> {formatDate(rent.expectedReturnDate)}{" "}
+                      </p>
+                    </div>
+                  </div>
+                  <p
+                    className="self-center justify-self-start text-lg font-semibold sm:w-[130px] sm:text-xs"
+                    style={{ color: Constants.STATUS_COLOR[rent.status] }}
+                  >
+                    {ERentStatus[rent.status]}
+                  </p>
+                  <Button
+                    variant="secondary"
+                    className="self-center text-xs text-muted-foreground"
+                    asChild
+                  >
+                    <Link href={`/rent/${rent.id}/details`}>Ver Detalhes</Link>
+                  </Button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      </Page>
     </>
   );
 };
