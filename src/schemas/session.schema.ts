9999fd2078a2ee3ba8c4b4714c1b6081d2d79667
Tarcisio <tarcisioandrade016@gmail.com>
2024-10-24 10:52:46 -0300

    feat: new validate in signup schema property name

--- src/schemas/session.schema.ts
+++ src/schemas/session.schema.ts
@@ -5,22 +5,16 @@ const signinSchema = z.object({
   password: z
     .string()
     .min(6, "A senha deve conter no minimo 6 digitos.")
-    .regex(
-      new RegExp("[a-zA-Z]"),
-      "A senha precisa conter no mínimo uma letra.",
-    ),
+    .regex(new RegExp("[a-zA-Z]"), "A senha precisa conter no mínimo uma letra."),
 });
 
 const signupSchema = z.object({
-  name: z.string(),
+  name: z.string().refine((value) => !value.match("[0-9]"), "O nome não pode conter números."),
   email: z.string().email("E-mail inválido."),
   password: z
     .string()
     .min(6, "A senha deve conter no minimo 6 digitos.")
-    .regex(
-      new RegExp("[a-zA-Z]"),
-      "A senha precisa conter no mínimo uma letra.",
-    ),
+    .regex(new RegExp("[a-zA-Z]"), "A senha precisa conter no mínimo uma letra."),
   // Foi adicionando duas barras no regexp do telefone, pois o prettier estava modificando a string.
   // Original Regex = (\b\(\d{2}\)\s?[9]?\s?\d{4}(\-|\s)?\d\d{4})|(\b\d{2}\s?[9]?\s?\d{4}(\-|\s)?\d{4})|(\b([9]|[9]\s)?\d{4}(\-|\s)?\d{4})|(\b\d{4}(\-|\s)?\d{4})
   telephone: z
